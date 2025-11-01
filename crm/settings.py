7766b0717e1a7f2a799710202b972a01d1a81297
KiptooStephen <kiptoos@gmail.com>
2025-11-01 20:33:35 +0300

    Update settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -3,27 +3,18 @@
 INSTALLED_APPS = [
     # ...
     "django_celery_beat",
-    # ...
 ]
-from celery.schedules import crontab
-# Celery broker & backend
+
 CELERY_BROKER_URL = "redis://localhost:6379/0"
 CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
-
-# Optional: so tasks run in local timezone
-CELERY_ENABLE_UTC = False
 CELERY_TIMEZONE = "Africa/Nairobi"
+CELERY_ENABLE_UTC = False
 
-
+from celery.schedules import crontab
 
 CELERY_BEAT_SCHEDULE = {
     "generate-crm-report": {
         "task": "crm.tasks.generate_crm_report",
-        # Monday 06:00 (Nairobi time, since we set CELERY_TIMEZONE)
         "schedule": crontab(day_of_week="mon", hour=6, minute=0),
-        # optional: pass kwargs
-        "options": {
-            "queue": "default",
-        },
     },
 }
