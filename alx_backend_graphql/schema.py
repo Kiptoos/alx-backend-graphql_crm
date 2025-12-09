2a07db6bfc66b262eda56d94dc6c5a90b1c98fa4
KiptooStephen <kiptoos@gmail.com>
2025-12-09 12:31:18 +0300

    Extend Query class to inherit from CRMQuery

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,9 +1,12 @@
 import graphene
+from crm.schema import Query as CRMQuery
 
-class Query(graphene.ObjectType):
+
+class Query(CRMQuery, graphene.ObjectType):
     hello = graphene.String()
 
     def resolve_hello(root, info):
         return "Hello, GraphQL!"
+
 
 schema = graphene.Schema(query=Query)
